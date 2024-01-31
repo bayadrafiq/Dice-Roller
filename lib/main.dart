5d45e1df425cc65f6e781f8640dd1e30988151df
Bayad <bayad999.bd@gmail.com>
2024-01-31 10:28:58 +0300

    codes

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Center(child: Text('Dicee')),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Row(children: 
+        [Expanded(
+          child: TextButton (onPressed:(){print('left button pressed'); },
+           child: Image.asset('images/dice1.png'),)
+        ),
+        Expanded(child: TextButton(onPressed:(){print('right button pressed');},
+        child: Image.asset('images/dice2.png'),)
+          
+        ),
+        ]),
+      ),
+    );
+  }
+}
